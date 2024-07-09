52013efcaffb24d61d06d472198a540df455e49a
benAloni <146990293+benAloni@users.noreply.github.com>
2024-07-09 16:21:02 +0300

    Update GiftForEmployee.jsx

--- fronted/src/views/pages/Employees/ForEmployee/GiftForEmployee.jsx
+++ fronted/src/views/pages/Employees/ForEmployee/GiftForEmployee.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
 import Sidebar from '../../../layout/Sidebar'
 import Header from '../../../layout/Header'
 export default function GiftForEmployee() {
@@ -15,13 +14,5 @@ export default function GiftForEmployee() {
     </div>
     {/* </div> */}
 </>
-=======
-
-export default function GiftForEmployee() {
-  return (
-    <div>
-   BYME
-    </div>
->>>>>>> a7de5759a9bef78c06b46c0602cbfd7a231c1538
   )
 }
